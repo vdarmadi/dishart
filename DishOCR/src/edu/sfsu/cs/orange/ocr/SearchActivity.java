28a346262e27e450a055c816de0add6196a9edcb
vdarmadi <victor.darmadi@gmail.com>
2013-07-25 23:06:02 -0700

    fixing the listview.

--- DishOCR/src/edu/sfsu/cs/orange/ocr/SearchActivity.java
+++ DishOCR/src/edu/sfsu/cs/orange/ocr/SearchActivity.java
@@ -26,6 +26,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -90,7 +91,27 @@ public class SearchActivity extends Activity {
 				}
 			}
 		});
+		listview.setOnTouchListener(new ListView.OnTouchListener() {
+	        @Override
+	        public boolean onTouch(View v, MotionEvent event) {
+	            int action = event.getAction();
+	            switch (action) {
+	            case MotionEvent.ACTION_DOWN:
+	                // Disallow ScrollView to intercept touch events.
+	                v.getParent().requestDisallowInterceptTouchEvent(true);
+	                break;
 
+	            case MotionEvent.ACTION_UP:
+	                // Allow ScrollView to intercept touch events.
+	                v.getParent().requestDisallowInterceptTouchEvent(false);
+	                break;
+	            }
+
+	            // Handle ListView touch events.
+	            v.onTouchEvent(event);
+	            return true;
+	        }
+	    });
 		searchButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {

c0e902b5f3e4f2ad0142dad809aa414e14d2192f
vdarmadi <victor.darmadi@gmail.com>
2013-10-06 23:06:59 -0700

    maybe solve the not up-to-date position.

--- DishOCR/src/edu/sfsu/cs/orange/ocr/GPSTracker.java
+++ DishOCR/src/edu/sfsu/cs/orange/ocr/GPSTracker.java
@@ -180,6 +180,8 @@ public class GPSTracker extends Service implements LocationListener {
  
     @Override
     public void onLocationChanged(Location location) {
+    	longitude = location.getLongitude();
+        latitude = location.getLatitude();
     }
  
     @Override

299be47465f0c32e173bf5b571e820ae9623ccdb
vdarmadi <victor.darmadi@gmail.com>
2013-10-15 22:54:13 -0700

    decrease update location

--- DishOCR/src/edu/sfsu/cs/orange/ocr/GPSTracker.java
+++ DishOCR/src/edu/sfsu/cs/orange/ocr/GPSTracker.java
@@ -15,6 +15,7 @@ import android.util.Log;
 
 public class GPSTracker extends Service implements LocationListener {
 	 
+	private static final String TAG = GPSTracker.class.getSimpleName();
     private final Context mContext;
  
     // flag for GPS status
@@ -31,10 +32,10 @@ public class GPSTracker extends Service implements LocationListener {
     double longitude; // longitude
  
     // The minimum distance to change Updates in meters
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
  
     // The minimum time between updates in milliseconds
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
+    private static final long MIN_TIME_BW_UPDATES = 1000; // 1 minute
  
     // Declaring a Location Manager
     protected LocationManager locationManager;
@@ -59,6 +60,7 @@ public class GPSTracker extends Service implements LocationListener {
  
             if (!isGPSEnabled && !isNetworkEnabled) {
                 // no network provider is enabled
+            	Log.e(TAG,"no network provider is enabled");
             } else {
                 this.canGetLocation = true;
                 // First get location from Network Provider
